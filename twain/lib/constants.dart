8259f293f1511c1c15b3684e7660a17300cf2a14
Dhivakar K <dhivakar189@gmail.com>
2021-05-05 10:29:04 +0530

    Added Progress

--- twain/lib/constants.dart
+++ twain/lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const Color kPowder = Color(0xFFF7F9F6);
-const String url = '192.168.2.6:5000';
+const String url = 'flaskadmin:flaskterflut258@twain.pythonanywhere.com';
 const Color kCream = Color(0xFFEEF3EC);
 const Color kCamblue = Color(0xFF94C4B2);
 const Color kOpal = Color(0xFFAECBC3);
